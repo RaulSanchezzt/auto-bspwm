24baf4de772ddce8fa6c5962f536226852b3b749
RaulSanchezzt <rzzt@duck.com>
2022-02-03 22:43:12 +0100

    🛑 HTB script deleted

--- config/polybar/scripts/hackthebox_status.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
- 
-IFACE=$(/usr/sbin/ifconfig | grep tun0 | awk '{print $1}' | tr -d ':')
- 
-if [ "$IFACE" = "tun0" ]; then
-    echo " $(/usr/sbin/ifconfig tun0 | grep "inet " | awk '{print $2}')%{u-}"
-else
-    echo " Disconnected"
-fi
