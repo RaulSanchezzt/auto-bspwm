6f4150455470fc0aa2e55377f7586c5c75306a9a
RZZT <raulsanchezzt@duck.com>
2022-06-03 18:51:34 +0200

    📰 Add Zettlr

--- setup.sh
+++ setup.sh
@@ -17,8 +17,7 @@ sudo apt update -y
 sudo apt install -y git kitty firejail wmname manpages-dev python3 python3-dev arandr scrub flameshot arc-theme fzf ripgrep universal-ctags silversearcher-ag fd-find xclip xsel zsh zsh-autosuggestions zsh-syntax-highlighting feh bspwm sxhkd polybar rbenv htop lxappearance python3-pip unclutter  papirus-icon-theme imagemagick bat ranger watchman libfreetype6-dev libfontconfig1-dev flatpak
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepoif
-flatpak install flathub it.mijorus.smile
-
+flatpak install -y flathub it.mijorus.smile com.zettlr.Zettlr
 # DEPENDENCIES
 
 # bspwm
